e4e1d51f0e4da503c5318b011e9bc438bd5114a8
UnsignedByte <edlam1889@gmail.com>
2020-08-23 20:08:14 -0700

    EXPLODE

--- js/POSTPISCHIL/access.js
+++ js/POSTPISCHIL/access.js
@@ -2,7 +2,7 @@
 * @Author: UnsignedByte
 * @Date:   18:12:54, 23-Aug-2020
 * @Last Modified by:   UnsignedByte
-* @Last Modified time: 19:43:41, 23-Aug-2020
+* @Last Modified time: 20:06:44, 23-Aug-2020
 */
 
 const fetch = require('node-fetch')
@@ -11,23 +11,48 @@ const fs = require('fs');
 
 let uuid = 298225;
 const delay = ms => new Promise(res => setTimeout(res, ms));
+const filter = "!Fxzr.hTsTuASi98IBjG))qEF.("
 
-async function getCommentIDs(uuid){
+async function continuousFetch(link, parseF){
 	let page = 0;
-	let comments = [];
+	let data = [];
 	let response;
 	do {
-		response = await fetch(`https://api.stackexchange.com/2.2/users/${uuid}/comments?order=desc&sort=creation&site=stackoverflow&pagesize=100&page=${++page}&filter=!babFNN1pR4yfqo`).then(x=>x.json())
+		response = await fetch(`${link}?order=desc&sort=creation&site=stackoverflow&pagesize=100&page=${++page}&filter=${filter}`).then(x=>x.json())
 		if (response.error_id) {
 			throw new Error(`Server responded with status ${response.error_id} and message ${response.error_message}.`);
 		}
+		if (response.backoff) {
+			console.log(`Backoff recieved, waiting ${response.backoff} seconds.`);
+			await delay(response.backoff*1000);
+		}
 
-		comments = comments.concat(response.items.map(x=>({body:x.body, date:new Date(x.creation_date*1000), score:x.score, id:x.comment_id})));
-		console.log(`${comments.length} comments fetched.`);
+		data = data.concat(response.items.map(parseF));
+		console.log(`${data.length} fetched.`);
 	} while (response.has_more)
 	
-	return comments;
+	return data;
 }
 
-getCommentIDs(uuid)
-	.then(x=>fs.promises.writeFile(path.resolve(__dirname, `./data.json`), JSON.stringify(x)))
+async function getComments(uuid){
+	return await continuousFetch(
+		`https://api.stackexchange.com/2.2/users/${uuid}/comments`,
+		x=>({body:x.body, markdown:x.body_markdown, date:new Date(x.creation_date*1000), score:x.score, id:x.comment_id})
+	)
+}
+
+async function getPosts(uuid){
+	return await continuousFetch(
+		`https://api.stackexchange.com/2.2/users/${uuid}/posts`,
+		x=>({body:x.body, markdown:x.body_markdown, type:x.post_type, date:new Date(x.creation_date*1000), score:x.score, id:x.post_id})
+	)
+}
+
+(async () => {
+	let data = {};
+	data.posts = await getPosts(uuid, "post");
+	data.comments = await getComments(uuid, "comment");
+
+	fs.promises.writeFile(path.resolve(__dirname, `./data.json`), JSON.stringify(data, null, 2))
+})();
+
