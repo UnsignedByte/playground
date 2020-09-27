a3f64fc5bb149176665a30d4c8a0490a8b75c661
UnsignedByte <edlam1889@gmail.com>
2020-09-27 10:50:56 -0700

    test

---
+++ cpp/includeif.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <algorithm>
+#include <set>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+
+#define INF 2147483647
+#define LLINF 9223372036854775807
+#define EPS 10e-9
+#define HASHPRIME int(1e9+7)
+
+#include "test.h"
+#ifdef test_exists
+void ra(){
+	cout << "hi" << endl;
+}
+#else
+void ra(){
+	cout << "no" << endl;
+}
+#endif
+
+
+int main(){
+	ra();
+}
