9fdab645a7c05214519c1f9bc0242d4a11efd049
UnsignedByte <edlam1889@gmail.com>
2022-01-28 12:41:39 -0800

    quine :(

--- ocaml/helloworld.ml
+++ ocaml/helloworld.ml
@@ -2,7 +2,7 @@
 * @Author: UnsignedByte
 * @Date:   2022-01-26 18:34:14
 * @Last Modified by:   UnsignedByte
-* @Last Modified time: 2022-01-28 11:47:49
+* @Last Modified time: 2022-01-28 12:28:34
 *)
 
 let () = print_endline "Hello World!"
@@ -14,6 +14,6 @@ let () = print_endline (sus "mmm")
 (* open String *)
 let () = print_endline (let y, x = "\" in x ^ (String.escaped y) ^ \"\\\", \\\"\" ^ String.escaped x ^ y)", "let () = print_endline (let y, x = \"" in x ^ (String.escaped y) ^ "\", \"" ^ String.escaped x ^ y)
 
-let () = Printf.printf "%S" "let () = print_endline \"Hello World!\""
+let () = Printf.printf "%S" "let () = print_endline \"Hello World!\"";;
 
-let () = (fun s -> Printf.printf "%s %S" s s) "let () = (fun s -> Printf.printf \"%s %S\" s s)"
+(fun s->Printf.printf"%s%S"s s)"(fun s->Printf.printf\"%s%S\"s s)"
